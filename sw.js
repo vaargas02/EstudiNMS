02466b48c2db93f2ac7ff74188a7a6e1f54f3c94
vaargas02 <vargasverdaguer@gmail.com>
2026-09-23 19:12:04 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,20 @@
+// Raíz y nervio: service worker (cache-first, offline total)
+const CACHE = 'raiz-nervio-v1.0.0';
+const FILES = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png', './apple-touch-icon.png'];
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)).then(() => self.skipWaiting()));
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+});
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request, { ignoreSearch: true }).then(hit => hit || fetch(e.request).then(res => {
+      if (res.ok && new URL(e.request.url).origin === location.origin) {
+        const copy = res.clone(); caches.open(CACHE).then(c => c.put(e.request, copy));
+      }
+      return res;
+    }).catch(() => e.request.mode === 'navigate' ? caches.match('./index.html') : undefined))
+  );
+});
